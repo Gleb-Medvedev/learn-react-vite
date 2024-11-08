8ac641317ce489be5f953451bfade0a591051621
Gleb-Medvedev <4e4enell@gmail.com>
2024-11-08 23:25:35 +0300

    learned 2way binding + styling on conditions

--- src/assets/components/FeedbackSection/FeedbackSection.jsx
+++ src/assets/components/FeedbackSection/FeedbackSection.jsx
@@ -3,11 +3,19 @@ import { Button } from "../Button/Button"
 
 export function FeedbackSection() {
 
-    const [text, setText] = useState('');
+    const [text, setText] = useState('test value');
     const [reason, setReason] = useState('suggestion');
+
+    const [hasError, setHasError] = useState(false)
 
     function inputTextChange(event) {
         setText(event.target.value)
+        setHasError(event.target.value.trim().length < 2 ? true : false)
+        console.log(hasError);        
+    }
+
+    function qwe(event) {
+        setReason(event.target.value);     
     }
 
     return (
@@ -15,10 +23,10 @@ export function FeedbackSection() {
             <h3>Обратная связь</h3>
             <form>
                 <label htmlFor="text" style={{marginBottom: '24px'}}>Введите текст:</label>
-                <input type="text" id="text" className="input" style={{marginBottom: '24px'}} value={text} onChange={inputTextChange} />
+                <input type="text" id="text" className="input" style={{marginBottom: '24px', outline: hasError ? '4px solid red' : 'none'}} value={text} onChange={inputTextChange} />
 
                 <label htmlFor="reason" style={{marginBottom: '24px'}}>Причина обращения</label>
-                <select id="reason" style={{marginBottom: '24px'}}>
+                <select id="reason" style={{marginBottom: '24px'}} value={reason} onChange={qwe}>
                     <option value="error">Ошибка</option>
                     <option value="help">Нужна помощь</option>
                     <option value="suggestion">Есть предложение</option>
